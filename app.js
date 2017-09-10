4b13cc3361e670067f3218fe34373c6a06a64283
Scottie Schneider <scott@laclave.co>
2017-09-10 11:47:54 -0500

    Commit 1 Added mongoose, beginning schema work

--- app.js
+++ app.js
@@ -2,13 +2,31 @@
 let express = require('express');
 let app = express();
 let routes = require('./routes');
+let logger = require('morgan');
 
 let jsonParser = require('body-parser').json;
 
-
+app.use(logger('dev'));
 app.use(jsonParser());
 
 app.use('/questions', routes);
+
+// catch 404 and forward to error handler
+app.use( (req, res, next) => {
+	let err = new Error('Not found!');
+	err.status = 404;
+	next(err);
+})
+
+// Error Handler 
+app.use( (err, req, res, next) => {
+	res.status(err.status || 500);
+	res.json({
+		error: {
+			message: err.message
+		}
+	})
+})
 
 let port = process.env.PORT || 3000;
 
